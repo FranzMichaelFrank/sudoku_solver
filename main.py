c93c09a8def634b26773328b2a2d6d56501b9537
FranzMichaelFrank <76612050+FranzMichaelFrank@users.noreply.github.com>
2021-05-26 21:45:57 +0100

    final version

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from charles.charles import Population, Individual
-from charles.search import hill_climb, sim_annealing
 from charles.selection import fps, tournament, rank
 from charles.mutation import swap_mutation, binary_mutation, inversion_mutation, distinct_mutation
 from charles.crossover import cycle_co, single_point_co, arithmetic_co, pmx_co
@@ -13,12 +12,21 @@ from copy import deepcopy
 
 
 def evaluate(sudoku, initial_sudoku):
-    """Describe function
+    """This fitness function computes fitness scores for solved Sudokus. For each row, column and block the score is increased
+        with the respective number of distinct digits within that particular row, column or block. The maxmimum score that can be 
+        reached is 243.
+
+    Args:
+        sudoku: a list that corresponds to a solution for a Sudoku puzzle
+        initial_sudoku: a list that corresponds to an unsolved Sudoku puzzle for which "sudoku" is the solution
+
 
     Returns:
+        fitness_score (int): a fitness score for a Sudoku solution
         
     """
     
+    #build the full Sudoku matrix from the given puzzle and solution
     sud_full = []
     count_sol = 0
     for element in initial_sudoku:      
@@ -33,21 +41,16 @@ def evaluate(sudoku, initial_sudoku):
 
     #get fitness of rows
     for row in range(9):
-        if len(set(sudoku_matrix[row])) == 9:
-            fitness_score += len(set(sudoku_matrix[row])) + 1
-        else:
-            fitness_score += len(set(sudoku_matrix[row]))
+        
+        fitness_score += len(set(sudoku_matrix[row]))
 
     #get fitness of columns
     for col in range(9):
         column = []
         for row in range(9):
             column.append(sudoku_matrix[row][col])
-
-        if len(set(column)) == 9:
-            fitness_score += len(set(column)) + 1
-        else:
-            fitness_score += len(set(column))
+        
+        fitness_score += len(set(column))
 
     #get fitness of blocks
     for element in [[sudoku_matrix[i][j] for i in range(0,3) for j in range(0,3)], [sudoku_matrix[i][j] for i in range(3,6) for j in range(0,3)],
@@ -55,35 +58,21 @@ def evaluate(sudoku, initial_sudoku):
                     [sudoku_matrix[i][j] for i in range(3,6) for j in range(3,6)], [sudoku_matrix[i][j] for i in range(6,9) for j in range(3,6)],
                     [sudoku_matrix[i][j] for i in range(0,3) for j in range(6,9)], [sudoku_matrix[i][j] for i in range(3,6) for j in range(6,9)],
                     [sudoku_matrix[i][j] for i in range(6,9) for j in range(6,9)]]:
-        if len(set(element)) == 9:
-            fitness_score += len(set(element)) + 1
-        else:
-            fitness_score += len(set(element))
+        
+        fitness_score += len(set(element))
     
     return fitness_score
 
-
+#there is no need for a neighbourhood function for the underlying problem
 def get_neighbours(self):
-    """A neighbourhood function for the TSP problem. Switches
-    indexes around in pairs.
-
-    Returns:
-        list: a list of individuals
-    """
-    n = [deepcopy(self.representation) for i in range(len(self.representation) - 1)]
-
-    for count, i in enumerate(n):
-        i[count], i[count + 1] = i[count + 1], i[count]
-
-    n = [Individual(i) for i in n]
-    return n
+    pass
 
 # Monkey patching
 Individual.evaluate = evaluate
 Individual.get_neighbours = get_neighbours
 
 # Initialize a sudoku and a population of random solutions for it
-pop_size = 100
+pop_size = 200000
 sudoku_and_population = initialize_population(pop_size,"easy")
 
 pop = Population(
@@ -91,19 +80,30 @@ pop = Population(
     optim="max",
     individuals=sudoku_and_population["population"],
     initial_sudoku=sudoku_and_population["sudoku"],
-    #**kwargs
-    #dynamic length of solution
 )
 
+#evolve the population
 pop.evolve(
-    gens=100,
-    select=rank, #works for all three
-    crossover=single_point_co, #works only for single_point_co, cycle_co, single_point_co, pmx_co
-    mutate=distinct_mutation, #works for both
-    co_p=0.7,
-    mu_p=0.2,
+    gens=75,
+    select=tournament,
+    crossover=pmx_co,
+    mutate=swap_mutation,
+    co_p=0.8,
+    mu_p=0.7,
     elitism=True
 )
 
+#display the empty Sudoku puzzle
+display_sudoku(sudoku_and_population["sudoku"])
 
-display_sudoku(pop[0], sudoku_and_population["sudoku"])
+#get the solution with the highest fitness score
+max_position = 0
+max_fitness = 0
+
+for j in range(pop_size):
+    if pop[j].fitness > max_fitness:
+        max_position = j
+        max_fitness = pop[j].fitness
+
+#display the solved Sudoku puzzle
+display_sudoku(pop[max_position], sudoku_and_population["sudoku"])
